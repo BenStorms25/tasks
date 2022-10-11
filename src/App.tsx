631650eb00c58269e03685f264780192a3ea0ef1
Ben Storms <ben.storms2@gmail.com>
2022-10-11 14:43:31 -0400

    added hello world

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript with Ben
+                COS420 with React Hooks and TypeScript with Ben
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <body>
+                <p>Hello World!</p>
+            </body>
         </div>
     );
 }
